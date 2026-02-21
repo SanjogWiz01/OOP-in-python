463e25f81877c4b6b29a919a223b786ec888f337
sanjogwiz01 <sanjogpoudel384@gmail.com>
2026-02-21 22:20:26 +0545

    abstractions files added

---
+++ Object Orinted Programming/Abstraction/08_abstractions.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+# Abstract class
+class Shape(ABC):
+
+    @abstractmethod
+    def area(self):
+        pass
+
+
+# Derived class
+class Rectangle(Shape):
+
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+
+# Main Program
+r = Rectangle(5, 3)
+print("Area of Rectangle =", r.area())
