83cfaaebbce193dd0a6520c088cc8ee0c0f1159e
sanjogwiz01 <sanjogpoudel384@gmail.com>
2026-02-21 22:24:29 +0545

    vechile_run.py

---
+++ Object Orinted Programming/Abstraction/10_vachile_pratice.py
@@ -0,0 +1,22 @@
+# file3_vehicle_abstraction.py
+
+from abc import ABC, abstractmethod
+
+# Abstract class
+class Vehicle(ABC):
+
+    @abstractmethod
+    def start(self):
+        pass
+
+
+# Derived class
+class Bike(Vehicle):
+
+    def start(self):
+        print("Bike Started")
+
+
+# Main Program
+b = Bike()
+b.start()
